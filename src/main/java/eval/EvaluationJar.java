9802f0b9803712351803cfb6df24744c29800f2b
Palaniappan Muthuraman <palaniappan.pp@gmail.com>
2025-07-11 14:26:42 +0200

    test class reformatted

--- src/main/java/eval/EvaluationJar.java
+++ src/main/java/eval/EvaluationJar.java
@@ -2,52 +2,53 @@ package eval;
 
 import api.SootFX;
 import com.google.common.base.Stopwatch;
+import core.fx.base.MethodFEU;
+import core.fx.base.WholeProgramFEU;
+import core.fx.wholeprogrambased.WholeProgramSinkCount;
+import core.fx.wholeprogrambased.WholeProgramSourceCount;
+import core.fx.wholeprogrambased.WholeProgramSourceSinkCount;
 import core.rm.ClassFeatureSet;
 import core.rm.MethodFeatureSet;
 import core.rm.WholeProgramFeatureSet;
 
 import java.io.*;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class EvaluationJar {
 
-//    public static void main(String[] args) throws IOException {
-//        for (int i = 74; i <= 74; i++) {
-//            String jarName = "mvn-" + i;
-//            String path = "/Users/kadiray/Workspace/maven-top/" + jarName + ".jar";
-//            String out = "/Users/kadiray/Workspace/SootFX/eval/jar/" + jarName + "/";
-//
-//            try {
-//                Stopwatch stopwatch = Stopwatch.createStarted();
-//
-//                methodFeatures(path, out);
-//                long methodDone = stopwatch.elapsed(TimeUnit.MILLISECONDS);
-//
-//                classFeatures(path, out);
-//                long classDone = stopwatch.elapsed(TimeUnit.MILLISECONDS);
-//
-//                wpFeatures(path, out);
-//                long wpDone = stopwatch.elapsed(TimeUnit.MILLISECONDS);
-//
-//                logMeta(out, methodDone, classDone - methodDone, wpDone - classDone, new File(path).length());
-//            } catch (Exception e){
-//                e.printStackTrace();
-//                System.err.println("error in apk:" + jarName);
-//            }
-//        }
-//    }
-
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
-        SootFX sootFX = new SootFX();
-        sootFX.addClassPath("/Users/palaniappanmuthuraman/WorkSpace/LinearConstantPropagation/IDELinearConstantAnalysisClientSootUp/src/test/resources/latest/commons-codec-1.15.jar"); //path to jar or apk file
-//        Set<MethodFeatureSet> featureSets = sootFX.extractAllMethodFeatures();
-//        sootFX.printMultiSetToCSV(featureSets, "/Users/palaniappanmuthuraman/WorkSpace/SootFX/method_output.csv"); //path to output csv file
-        WholeProgramFeatureSet wholeProgramFeatureSet = sootFX.extractAllWholeProgramFeatures();
-        sootFX.printSingleSetToCSV(wholeProgramFeatureSet, "/Users/palaniappanmuthuraman/WorkSpace/SootFX/whole_output.csv");
+        String path = "/Users/palaniappanmuthuraman/WorkSpace/LinearConstantPropagation/IDELinearConstantAnalysisClientSootUp/src/test/resources/latest/commons-codec-1.15.jar";
+        String out = "/Users/palaniappanmuthuraman/WorkSpace/SootFX/whole_output.csv";
+//        executeAllFeatures(path, out);
+        HashSet<WholeProgramFEU> wholeProgramFEUS = new HashSet<>();
+        wholeProgramFEUS.add(new WholeProgramSourceCount());
+        wholeProgramFEUS.add(new WholeProgramSinkCount());
+        wpFeaturesInclude(path, out, wholeProgramFEUS);
         long end = System.currentTimeMillis();
         System.out.println((end - start) / 1000);
+    }
+
+    public static void executeAllFeatures(String path, String out){
+        try {
+            Stopwatch stopwatch = Stopwatch.createStarted();
+
+            methodFeatures(path, out);
+            long methodDone = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+
+            classFeatures(path, out);
+            long classDone = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+
+            wpFeatures(path, out);
+            long wpDone = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+
+            logMeta(out, methodDone, classDone - methodDone, wpDone - classDone, new File(path).length());
+        } catch (Exception e){
+            e.printStackTrace();
+            System.err.println("error in jarFile:");
+        }
     }
 
     public static void logMeta(String path, long methodTime, long classTime, long wpTime, long fileSizeInBytes) throws IOException {
@@ -79,6 +80,14 @@ public class EvaluationJar {
         sootFX.appOnly();
         Set<MethodFeatureSet> featureSets = sootFX.extractAllMethodFeatures();
         sootFX.printMultiSetToCSV(featureSets, out + "method.csv");
+    }
+
+    public static void wpFeaturesInclude(String path, String out, Set<WholeProgramFEU> methodFEUSet) throws IOException {
+        SootFX sootFX = new SootFX();
+        sootFX.addClassPath(path);
+        sootFX.appOnly();
+        WholeProgramFeatureSet wholeProgramFeatureSet = sootFX.extractWholeProgramFeaturesInclude(methodFEUSet);
+        sootFX.printSingleSetToCSV(wholeProgramFeatureSet, out + "wp_include.csv");
     }
 
     public static void wpFeatures(String path, String out) throws IOException {
